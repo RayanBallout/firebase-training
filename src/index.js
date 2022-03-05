7efe99aaca67f778cd26b7390341584aa1863a91
RayanBallout <rayanballout14@gmail.com>
2022-03-05 12:13:27 +0200

    Adding the firebase config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello from index.js')
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCiZX7NKn7p5zz8VNVbjqWDudaJymsf-Og",
+	authDomain: "fir-9-training-903d7.firebaseapp.com",
+	projectId: "fir-9-training-903d7",
+	storageBucket: "fir-9-training-903d7.appspot.com",
+	messagingSenderId: "841615203332",
+	appId: "1:841615203332:web:45686fc6852647b7552951",
+}
+
+initializeApp(firebaseConfig)
